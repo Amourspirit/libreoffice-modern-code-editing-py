d2d8598ece158d97e29df5ebb827a1fa4b6346e3
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-06-12 21:49:57 -0400

    test and update readme

---
+++ src/hello_world/README.md
@@ -0,0 +1,11 @@
+# HELLO WORLD README
+
+The `hello_world.py` module can be run as a LibreOffice Python macro or directly in the code editor.
+
+When run this module simply writes `Hello World` into a LibreOffice Writer Document.
+
+When `hello_world()` macro is called in a LibreOffice Document then `Hello World` is written into that document.
+
+When the `hello_world.py` module is run from code editor it will start LibreOffice,
+Create a new Writer Document, and write `Hello World` into it.
+After a brief pause the LibreOffice is closed.
---
+++ tests/test_hello_world.py
@@ -0,0 +1,41 @@
+# import mock
+import pytest
+from ooodev.utils.lo import Lo
+from ooodev.office.write import Write
+from ooodev.utils.gui import GUI
+from src.hello_world import hello_world
+
+# This is just a sample test to show how to use pytest with LibreOffice
+# For hundreds of examples see: https://github.com/Amourspirit/python_ooo_dev_tools/tree/main/tests
+
+# region run direct
+
+# this is not necessary but allows to run this test directly from python or Editor
+if __name__ == "__main__":
+    pytest.main([__file__])
+
+# endregion run direct
+
+
+def test_hello_world(loader, monkeypatch: pytest.MonkeyPatch) -> None:
+    # The hello_world.py module has the ability to be run directly and debugged directly.
+    # This test is to demonstrate how to test the hello_world.py module using pytest.
+    # This is the recommended way to test python code that is intended to be run in LibreOffice.
+    if Lo.bridge_connector.headless:
+        pytest.skip("Test skipped in headless mode")
+    doc = Write.create_doc()
+    GUI.set_visible(visible=True, doc=doc)
+    Lo.delay(500)
+    # Mock the module XSCRIPTCONTEXT variable.
+    # the hello_world() method expect the XSCRIPTCONTEXT variable to be set.
+    monkeypatch.setattr(hello_world, "XSCRIPTCONTEXT", Lo.xscript_context, raising=False)
+
+    # run the hello_world() method.
+    hello_world.hello_world()
+
+    cursor = Write.get_cursor(doc)
+    cursor.gotoStart(False)
+    cursor.gotoEnd(True)
+    assert cursor.getText().getString() == "Hello World"
+
+    Lo.close_doc(doc)
